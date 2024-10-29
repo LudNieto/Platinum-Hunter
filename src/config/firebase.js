99c1862c75f20854d391b7ad2487f9d4b10e6343
jhongonzalezs <jhongonzalezs2001@hotmail.com>
2024-10-29 11:35:09 -0500

    Avance 2 aun tiene fallas de ejecucion

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,9 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import app from "firebase/app";
+import "firebase/firestore";
 
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey: "AIzaSyAlRNASxoOTW0wbnjrVkcsyNb4-N6hA6-0",
   authDomain: "platinum-hunter.firebaseapp.com",
@@ -15,6 +13,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+app.initializeApp(firebaseConfig);
+const db = app.firestore();
 
-module.exports = { auth, db };
+export {db}
