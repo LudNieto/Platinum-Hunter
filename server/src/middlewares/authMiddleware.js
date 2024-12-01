4f6ec09aaf7df2a15a72619056a41fbf231d04f1
Ludwing Nieto <96258581+LudNieto@users.noreply.github.com>
2024-12-01 17:38:01 -0500

    fix 1 authMiddleware

--- server/src/middlewares/authMiddleware.js
+++ server/src/middlewares/authMiddleware.js
@@ -1,7 +1,7 @@
 import admin from 'firebase-admin';
 
 export const verifyToken = async (req, res, next) => {
-    const token = req.headers['authorization'];
+    const token = req.headers.authorization?.split(' ')[1];
     if (!token) {
         return res.status(403).json({ error: 'No token provided.' });
     }
@@ -15,7 +15,7 @@ export const verifyToken = async (req, res, next) => {
 };
 
 export const isAdmin = async (req, res, next) => {
-    const token = req.headers['authorization'];
+    const token = req.headers.authorization?.split(' ')[1];
     if (!token) {
         return res.status(403).json({ error: 'No token provided.' });
     }
